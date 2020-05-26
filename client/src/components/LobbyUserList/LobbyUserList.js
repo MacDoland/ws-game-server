c7a853f6de977c95bed210b5522dd9fb214edada
Joe McDowall <joe.mcdowall@and.digital>
2020-05-26 13:40:19 +0100

    Hooking up participants in chat

--- client/src/components/LobbyUserList/LobbyUserList.js
+++ client/src/components/LobbyUserList/LobbyUserList.js
@@ -2,11 +2,14 @@ import React from 'react';
 import './LobbyUserList.scss';
 
 const LobbyUserList = ({ users = [] }) => {
+
+console.log('users', users);
+
     return (
        <div className="lobby-user-list">
 
             {
-                users.map((user, index) => <div key={'user-'+index} className="lobby-user-list__user"><span className="lobby-user-list__user__name">{user.name}</span></div>)
+                users.map((user, index) => <div key={'user-'+index} className="lobby-user-list__user"><span className="lobby-user-list__user__name">{user.name || 'unknown'}</span></div>)
             }
 
        </div>
--- client/src/selectors/index.js
+++ client/src/selectors/index.js
@@ -1,29 +1,43 @@
-export const lobbySelector = (state) => {
-    let lobby = {};
+export const currentParticipantsSelector = (state) => {
+  let participants = [];
+  let currentLobby = currentLobbySelector(state);
 
-    let lobbyIndex = state.lobbies.findIndex(lobby => lobby.id === state.lobbyId);
+    console.log(currentLobby);
 
-    if(lobbyIndex >= 0){
-        lobby = Object.assign({}, state.lobbies[lobbyIndex]);
-    }
+  if (currentLobby && currentLobby.participants) {
+    console.log('currentLobby.participants', currentLobby.participants);
+    participants = currentLobby.participants.map((participant) => {
+      return state.users.find((user) => user.id === participant);
+    });
 
-    return lobby;
-}
+    participants = participants.filter(
+      (participant) => typeof participant !== "undefined"
+    );
+  }
+
+  console.log('participants', participants)
+
+  return participants;
+};
 
 export const userSelector = (state) => {
-    let user = { name: '' };
+  let user = { name: "" };
 
-    if (typeof (state.userId) === 'string' || state.userId !== '') {
-        let index = state.users.findIndex(user => user.id === state.userId);
+  if (typeof state.userId === "string" || state.userId !== "") {
+    let index = state.users.findIndex((user) => user.id === state.userId);
 
-        if (index >= 0) {
-           user = state.users[index];
-        }
+    if (index >= 0) {
+      user = state.users[index];
     }
+  }
 
-    return user;
-}
+  return user;
+};
 
 export const currentLobbySelector = (state) => {
-    return state.lobbies.filter(lobby => lobby.participants.filter(participant => participant === state.userId).length > 0);
-}
+  return state.lobbies.find(
+    (lobby) =>
+      lobby.participants.filter((participant) => participant === state.userId)
+        .length > 0
+  );
+};
